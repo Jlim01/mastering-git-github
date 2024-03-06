a534dbbd789dce0efc4c799321c1687991469ee6
Jlim01 <asd57573450@gmail.com>
2024-03-06 22:10:31 +0900

    manager

--- test.spec.js
+++ test.spec.js
@@ -1,9 +1,9 @@
 describe('Default Test Set', ()=>{
-  it('test1 should be passed', ()=>{
+  it('test33 should be passed', ()=>{
     console.log('test1 passed')
   });
 
-	it('test2 should be passed', ()=>{
+	it('test3 should be passed', ()=>{
 		console.log('test2 passed')
 	});
 });
