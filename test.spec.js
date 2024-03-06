416c8c949247cace69df73ae0ba07c9d8f7d22e1
Jlim01 <asd57573450@gmail.com>
2024-03-06 22:09:37 +0900

    service

--- test.spec.js
+++ test.spec.js
@@ -1,9 +1,9 @@
 describe('Default Test Set', ()=>{
   it('test33 should be passed', ()=>{
     console.log('test1 passed')
-  });
+dsdf });
 
-	it('test3 should be passed', ()=>{
+	it('test3 sdzxcvxcvshould be passed', ()=>{
 		console.log('test2 passed')
 	});
 });
